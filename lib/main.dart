b7cf6e3f987cfe4a1b2e6656064a81c5f8bdeead
arifvtti <arif.vtti@gmail.com>
2023-03-10 17:02:10 +0600

    stateless widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,85 +1,22 @@
-import 'package:flutter/material.dart';
+import "package:flutter/material.dart";
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.green,
-      ),
-      home: const MyHomePage(title: 'Increment app with flutter'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  void _decrementCounter() {
-    setState(() {
-      if (_counter > 0) {
-        _counter--;
-      }
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'Welcome to counter application',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Padding(
-                    padding: EdgeInsets.all(10),
-                    child: ElevatedButton(
-                        onPressed: _decrementCounter,
-                        child: Icon(Icons.remove))),
-                Padding(
-                    padding: EdgeInsets.all(10),
-                    child: ElevatedButton(
-                        onPressed: _incrementCounter, child: Icon(Icons.add))),
-              ],
-            )
-          ],
+    return Center(
+      child: Container(
+        alignment: Alignment.center,
+        height: 200,
+        width: 200,
+        decoration: BoxDecoration(color: Colors.green),
+        child: Text(
+          "Hello Flutter",
+          textDirection: TextDirection.ltr,
+          style: TextStyle(color: Colors.white, fontSize: 20),
         ),
       ),
     );

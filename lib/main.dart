59000f6b405c946798dd6c69854cc59214023a44
arifvtti <arif.vtti@gmail.com>
2023-03-11 12:04:38 +0600

    safearea

--- lib/main.dart
+++ lib/main.dart
@@ -9,19 +9,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("App Bar"),
-          centerTitle: true,
-          leading: Icon(Icons.menu),
-          actions: [IconButton(onPressed: null, icon: Icon(Icons.add_a_photo))],
-        ),
-        body: Center(
-          child: Text(
-            "This is text widget",
-            style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-          ),
-        ),
+      home: SafeArea(
+        child: Scaffold(body: Text("Hello")),
       ),
     );
   }

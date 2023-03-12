4e2d71c8bbc5adb135a9d87e5bb2e437739d113c
arifvtti <arif.vtti@gmail.com>
2023-03-12 10:30:11 +0600

    card widget added

--- lib/main.dart
+++ lib/main.dart
@@ -29,6 +29,45 @@ class HomePage extends StatelessWidget {
 
     return Scaffold(
         appBar: AppBar(title: Text("Flutter")),
-        body: LiquidSwipe(pages: pages));
+        body: Center(
+          child: Card(
+            elevation: 10,
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(20))),
+            child: Container(
+              height: 250,
+              width: width - 20,
+              alignment: Alignment.topLeft,
+              child: Padding(
+                padding: const EdgeInsets.all(20),
+                child: Column(
+                  children: [
+                    Text(
+                      "Md Arif Mia,",
+                      style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.blue,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      "CEO & Founder",
+                      style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.blue,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      "ABC Ltd.",
+                      style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.blue,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ));
   }
 }

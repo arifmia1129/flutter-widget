3d94a09918e7437a0a97d5adf43251eec9b8f84f
arifvtti <arif.vtti@gmail.com>
2023-03-18 11:48:11 +0600

    show modal bottom sheet

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
 import "package:flutter/material.dart";
-import 'package:image_picker/image_picker.dart';
-import "dart:io";
 
 void main() => runApp(MyApp());
 
@@ -10,65 +8,48 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
       home: HomePage(),
     );
   }
 }
 
-class HomePage extends StatefulWidget {
+class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  final ImagePicker _picker = ImagePicker();
-  var imageSrc;
-
-  Future handleTakePictureFromCamera() async {
-    var src = await _picker.pickImage(source: ImageSource.camera);
-
-    setState(() {
-      imageSrc = src;
-    });
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        children: [
-          Container(
-            height: 500,
-            width: double.infinity,
-            color: Colors.blue,
-            child: imageSrc == Null
-                ? Image.file(imageSrc)
-                : Center(child: Text("Image not picked")),
-          ),
-          Divider(),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              FloatingActionButton(
-                onPressed: () {
-                  handleTakePictureFromCamera();
-                },
-                child: Icon(Icons.camera),
-              ),
-              SizedBox(
-                width: 20,
-              ),
-              FloatingActionButton(
-                onPressed: null,
-                child: Icon(Icons.photo_album),
-              )
-            ],
-          )
-        ],
-      ),
-    );
+    return Scaffold(
+        body: Center(
+      child: TextButton(
+          onPressed: () {
+            showModalBottomSheet(
+                context: context,
+                builder: (context) {
+                  return Container(
+                    height: 250,
+                    child: Column(
+                      children: [
+                        ListTile(
+                          title: Text("One"),
+                          subtitle: Text("This is one"),
+                          trailing: Icon(Icons.alarm),
+                        ),
+                        ListTile(
+                          title: Text("One"),
+                          subtitle: Text("This is one"),
+                          trailing: Icon(Icons.alarm),
+                        ),
+                        ListTile(
+                          title: Text("One"),
+                          subtitle: Text("This is one"),
+                          trailing: Icon(Icons.alarm),
+                        ),
+                      ],
+                    ),
+                  );
+                });
+          },
+          child: Text("Click me")),
+    ));
   }
 }
